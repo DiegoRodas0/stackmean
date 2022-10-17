15a371a8a852880540cc4dac2c06110705579910
DiegoRodas0 <rodasestradad01@gmail.com>
2022-10-16 22:03:23 -0600

    ejercicios de js 16-10-22

---
+++ bootstrap/js/js/K-texto.js
@@ -0,0 +1,49 @@
+var textoA = "segun su constitucion, es una republica"
+var textoB = "organizacion para su administracion en 22 departamentos "
+
+
+
+var resultado= textoA.toUpperCase()
+var resultado= textoB.toUpperCase()
+resultado= ["guatemala","salvador","xxx"]
+
+
+resultado2= texto.concat(" " + textoB)
+
+var busqueda = textoA.indexOf("una")
+var busqueda2 = textoA.lastIndexOf("una")
+var busqueda2 = textoA.search("una")
+var busqueda2 = textoA.match(/una/g)
+
+var busqueda2 = textoB = textoB.substr(19,14)
+var busqueda2 = textoB = textoB.charAt(19,)
+var busqueda2 = textoB = textoB.startsWith("org")
+var busqueda2 = textoB = textoB.charAt(19,)
+var busqueda2 = textoB = textoB.startsWith("departamentos")
+var cambiodeTexto = textoB = textoB.includes("administracion", "Guatemala")
+var cambiodeTexto = textoB = textoB.slice(5)
+var cambiodeTexto = textoB = textoB.split(" ")
+
+console.log(busqueda)
+console.log(busqueda2)
+console.log(busqueda3)
+console.log(busqueda4)
+console.log(cambiodeTexto)
+
+/*
+if(resultado.length > 0){
+console.log("tiene datos");
+}else{
+console.log("no tiene datos");
+}
+
+
+
+console.log(resultado.length);
+console.log(resultado);
+*/
+
+//que pasaria si yo quisiera identificar alguna palabra en algun texto?
+//KDE CUAL ES LA ESTRUCTURA DE LOS FICHEREOS DE LINIX 
+//LOS ENLACES SIMBOLICOS ES UNA DIRECCION QUE APUNTA UN ARCHIVO  
+//
